5c7f79325988863a854d1eae0879b18249a66c5c
yash mohite <114924851+mohiteyashprogrammer@users.noreply.github.com>
2023-10-28 14:39:45 +0530

    updatedall

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'helmet_detection',
+    version= '0.0.0',
+    author= 'yash mohite',
+    author_email= 'mohite.yassh@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
